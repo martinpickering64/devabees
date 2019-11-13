77f05bacd55982d032b8d70ee7445f632e825530
Martin Pickering <mail@martinpickering.com>
2019-11-13 23:32:29 +0000

    Updated to bootstrap v4 breaking changes

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -1,23 +1,16 @@
-// global. currently active menu item 
 var current_item = 0;
-
-// few settings
 var section_hide_time = 1300;
 var section_show_time = 1300;
-
-// jQuery stuff
 jQuery(document).ready(function($) {
-
-	// Switch section
-	$("a", '.mainmenu').click(function() 
+	$('li.nav-item').click(function() 
 	{
 		if( ! $(this).hasClass('active') ) { 
 			current_item = this;
-			// close all visible divs with the class of .section
+                        $(".navbar-collapse").collapse('hide');
 			$('.section:visible').fadeOut( section_hide_time, function() { 
-				$('a', '.mainmenu').removeClass( 'active' );  
+				$('li.nav-item').removeClass( 'active' );  
 				$(current_item).addClass( 'active' );
-				var new_section = $( $(current_item).attr('href') );
+				var new_section = $( $('a', current_item).attr('href') );
 				new_section.fadeIn( section_show_time );
 			} );
 		}
